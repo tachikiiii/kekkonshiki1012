def128dbb97c4eeeafd1c01f58af69526b3c3685
Hitomi Tachiki <rightg13948@gmail.com>
2025-07-12 19:00:31 +0900

    修正

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let isOpen = false;
 
+  
+
   hamburger.addEventListener("click", (e) => {
     e.stopPropagation(); // 他のクリックイベントとの干渉防止
 
     isOpen = !isOpen;
     if (isOpen) {
       navMenu.style.display = "flex";
-      menuImg.src = "close.png"; // ← メニュー開時の画像
-      menuImg.alt = "close";
     } else {
       navMenu.style.display = "none";
       menuImg.src = "menu.png";  // ← メニュー閉時の画像
@@ -33,4 +33,14 @@ document.addEventListener("DOMContentLoaded", () => {
       isOpen = false;
     }
   });
+});
+
+const image = document.getElementById("menu-icon");
+image.addEventListener("click", function () {
+  // 既に回転中なら一度外して再付与して何度でも回転できるように
+  image.classList.remove("spin");
+  // 次のフレームでclassを再追加
+  setTimeout(() => {
+    image.classList.add("spin");
+  }, 10);
 });
